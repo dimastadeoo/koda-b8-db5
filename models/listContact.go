1f6d9a4b32966d09f9ec04d9c7c35cf8e53c31bd
dimastadeoo <dimastadeoo@gmail.com>
2026-07-15 13:07:22 +0700

    feat: Add ListContact model and GetAllData function for retrieving contacts

---
+++ models/listContact.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type ListContact struct {
+	Id    int
+	Fullname  string
+	No_Hp int
+	Email string
+	Created_At time.Time
+	Updated_At time.Time
+}
+
+func GetAllData(conn *pgx.Conn) ([]ListContact, error) {
+
+	rows, _ := conn.Query(context.Background(), `
+		SELECT id, fullname, no_hp, email, created_at, updated_at
+		FROM list_contact
+	`)
+	lists, err := pgx.CollectRows(rows, pgx.RowToStructByName[ListContact])
+	return lists, err
+}
