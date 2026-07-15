e40ed06c6eee6a4c116897cd78ee1b1837f08206
dimastadeoo <dimastadeoo@gmail.com>
2026-07-15 18:31:00 +0700

    fix: Update .env.example and entrypoint.sh for improved environment variable handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 docker-entrypoint.sh postgres &
 
-# Write .env to /app/.env and keep POSTGRES_ prefix
-env | grep '^POSTGRES_' > /app/.env
+env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /app/.env
 
 until pg_isready -q; do sleep 1; done
 exec /app/program
